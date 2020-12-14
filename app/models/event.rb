0e0cfe23e6e51ea2001582fb91d8c187efffe5e1
aline utiyama <lineskutiyama@gmail.com>
2020-12-14 16:50:36 +0900

    Creating relational database

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :event_atendees, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable
+
+  has_many :professionals, dependent: :destroy
+  has_many :recycles, dependent: :destroy
+  has_many :events, dependent: :destroy
+  has_many :events, through: :event_atendees
 end
