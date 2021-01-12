f5db073ae1f3be46112314242ab2db5954734f3c
aline utiyama <lineskutiyama@gmail.com>
2021-01-12 13:26:21 +0900

    Professionals new page

--- app/controllers/professionals_controller.rb
+++ app/controllers/professionals_controller.rb
@@ -1,2 +1,33 @@
 class ProfessionalsController < ApplicationController
+
+  def index
+    @professionals = policy_scope(Professional)
+  end
+
+  def show
+    @professional = Professional.find(params[:id])
+  end
+
+  def new
+    @professional = Professional.new
+    authorize @professional
+  end
+
+  def create
+    @professional = Professional.new(professional_params)
+    @professional.user = current_user
+
+    if @professional.save
+      redirect_to professional_path(@professional)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def professional_params
+    params.require(:professional).permit(:full_name, :role, :details, :price, :email, :phone, :address)
+  end
+
 end
