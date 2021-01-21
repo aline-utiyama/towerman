92ac4eea3c43b6589f9b5b19b1d3885338c92e00
aline utiyama <lineskutiyama@gmail.com>
2021-01-21 20:25:17 +0900

    Setting info conditions

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -1,6 +1,6 @@
 class Professional < ApplicationRecord
   belongs_to :user
-  validates :full_name, :role, :details, :photo, presence: true
+  validates :full_name, :role, :details, :photo, :email, presence: true
   validates :details, length: { minimum: 3 }
   has_one_attached :photo, dependent: :destroy
 end
